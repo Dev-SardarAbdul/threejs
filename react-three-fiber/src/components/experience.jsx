f7e437b4deb51c0f0c714ba99a0c53a18140b5c3
Dev-SardarAbdul <dev.sardarabdul@gmail.com>
2024-01-29 04:52:10 +0500

    drei

--- react-three-fiber/src/components/experience.jsx
+++ react-three-fiber/src/components/experience.jsx
@@ -1,40 +1,67 @@
 import React from "react";
-import { useFrame, extend, useThree } from "@react-three/fiber";
+import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
-import { OrbitControls } from "three/examples/jsm/Addons";
+import {
+  OrbitControls,
+  PivotControls,
+  TransformControls,
+  Html,
+  Text,
+  Float,
+  MeshReflectorMaterial,
+} from "@react-three/drei";
 
 function Experience() {
   const cubeRef = useRef();
-  const groupRef = useRef();
-
-  extend({ OrbitControls });
-
-  const { camera, gl } = useThree();
+  const sphereRef = useRef();
 
   useFrame((state, delta) => {
     cubeRef.current.rotation.y += delta;
-    groupRef.current.rotation.y += delta;
   });
 
   return (
     <>
-      <orbitControls args={[camera, gl.domElement]} />
-      <directionalLight />
-      <group ref={groupRef}>
-        <mesh position-x={2} ref={cubeRef}>
-          <boxGeometry />
-          <meshStandardMaterial color={"orange"} />
-        </mesh>
+      <OrbitControls makeDefault />
+      <directionalLight intensity={5} />
+      <ambientLight intensity={2} />
+      <group>
+        <PivotControls
+          anchor={[0, 0, 0]}
+          fixed={true}
+          scale={100}
+          depthTest={false}
+        >
+          <mesh position-x={2} ref={cubeRef}>
+            <boxGeometry />
+            <meshStandardMaterial color={"black"} />
+          </mesh>
+        </PivotControls>
 
-        <mesh position-x={-2}>
+        <mesh position-x={-2} ref={sphereRef}>
           <sphereGeometry />
           <meshStandardMaterial color={"orange"} />
+          <Html center position={[1, 1, 0]} occlude="blending">
+            This is a text
+          </Html>
         </mesh>
+        <TransformControls object={sphereRef} />
       </group>
       <mesh scale={10} rotation-x={-Math.PI * 0.5} position-y={-1}>
         <planeGeometry />
-        <meshStandardMaterial color={"green"} />
+        {/* <meshStandardMaterial color={"green"} /> */}
+        <MeshReflectorMaterial
+          color={"green"}
+          blur={[1000, 1000]}
+          mixBlur={0.5}
+          resolution={1024}
+        />
       </mesh>
+
+      <Float speed={10}>
+        <Text position-y={3} color={"green"} maxWidth={4} textAlign="center">
+          I Love R3R
+        </Text>
+      </Float>
     </>
   );
 }
